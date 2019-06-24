83e81e0a9e6f24ab1c6c39736e3e583fceb7f4e8
ManuelVR461 <ManuelVR461@gmail.com>
2019-06-24 16:37:39 -0400

    SoftCole v1.1.3
    Se agrego modulo de Login

--- softcole/settings.py
+++ softcole/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from django.conf.global_settings import STATICFILES_DIRS
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -140,3 +142,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
+LOGIN_REDIRECT_URL = '/'
